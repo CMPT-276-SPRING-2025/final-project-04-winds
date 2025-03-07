2c0d0f5ab6455b7fde2178188cc304770c4af753
Arya Akhavein <aryaakhavein92@gmail.com>
2025-03-06 19:42:06 -0800

    Set Inria Sans as a global font

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import './App.css';
+import GlobalStyle from './GlobalStyle';
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <Header></Header>
-    </div>
+    <>
+      <GlobalStyle />
+      <Header />
+    </>
   );
-}
+};
 
 const Header = () => {
   return (
-    <header style={{
-      textAlign: 'center',
-      color: '#85A573',
-      fontFamily: "'Inria Sans', sans-serif",
-      }}>
-
+    <header style={{ textAlign: 'center',color: '#85A573',}}>
       <h1 style={{ fontSize: '3rem'}}>CookCompass</h1>
-
     </header>
   );
 };
+
 export default App;
---
+++ src/GlobalStyle.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Inria Sans', sans-serif;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    font-family: 'Inria Sans', sans-serif;
+  }
+
+  p, a, li, span {
+    font-family: 'Inria Sans', sans-serif;
+  }
+`;
+
+export default GlobalStyle;
