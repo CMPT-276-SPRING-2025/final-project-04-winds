ffbc3eaecd09439433527288ce79861052a11d8f
Arya Akhavein <aryaakhavein92@gmail.com>
2025-03-06 19:34:12 -0800

    Added Inria Sans font add title

--- src/App.js
+++ src/App.js
@@ -2,10 +2,23 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     
+    <div>
+      <Header></Header>
     </div>
   );
 }
 
+const Header = () => {
+  return (
+    <header style={{
+      textAlign: 'center',
+      color: '#85A573',
+      fontFamily: "'Inria Sans', sans-serif",
+      }}>
+
+      <h1 style={{ fontSize: '3rem'}}>CookCompass</h1>
+
+    </header>
+  );
+};
 export default App;
