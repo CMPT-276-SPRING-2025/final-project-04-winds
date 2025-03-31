68d23c1546ff0678af508003f4a9c7a5bf400359
soupsoup00 <hla342@sfu.ca>
2025-03-31 16:37:49 -0700

    added translate ui

---
+++ src/Title_Card/Translate.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import Languages from '../data/Languages';
+
+const Translate = ({}) => {
+
+    // const translateText = 
+        // ===== GOOGLE CLOUD CONFIG =====
+        const API_KEY = process.env.REACT_APP_GOOGLE_CLOUD_API_KEY;
+
+    // return(
+
+    // );
+};
+
+export default Translate;
