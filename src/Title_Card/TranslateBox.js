1a2e1e5b0942012b8823e254443e30e15f24a1cf
soupsoup00 <hla342@sfu.ca>
2025-03-31 16:43:36 -0700

    removed unused variable

--- src/Title_Card/TranslateBox.js
+++ src/Title_Card/TranslateBox.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect, lazy } from 'react';
+import React, { useState, useRef, useEffect} from 'react';
 import Languages from '../data/Languages';
 import './TranslateTTSBox.css'
 
