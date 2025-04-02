cca8c65e64adccd43655a14e49ec97cc4c1904a3
Arya <arya.akhavein@gmail.com>
2025-04-02 11:48:03 -0700

    added animation to the mic when audio is being recorded

--- src/Title_Card/TTS.js
+++ src/Title_Card/TTS.js
@@ -385,7 +385,7 @@ const TTS = ({analyzedInstructions}) => {
           disabled={isProcessing}      
           data-testid='listening-button'  
         >
-          <svg viewBox="0 0 24 24" width="16" height="16">
+           <svg viewBox="0 0 24 24" width="16" height="16" className={isListening ? "animate-pulse" : ""}>
             <path fill="currentColor" d="M12,2A3,3 0 0,1 15,5V11A3,3 0 0,1 12,14A3,3 0 0,1 9,11V5A3,3 0 0,1 12,2M19,11C19,14.53 16.39,17.44 13,17.93V21H11V17.93C7.61,17.44 5,14.53 5,11H7A5,5 0 0,0 12,16A5,5 0 0,0 17,11H19Z" />
           </svg>
           {isProcessing ? 'Processing...' : isListening ? 'Start Processing' : 'Start Listening'} 
